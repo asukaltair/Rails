b885d287408aa8d7ee981ca89da2d621988b90ec
asuka <asuka.altair@gmail.com>
2020-06-03 16:26:39 +0900

    [finish] 3-8

--- work/vagrant/sample_app/config/initializers/application_controller_renderer.rb
+++ work/vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '0e6daa819218f8e80768fd4d0983f525307807910eb93cbe5220e8f3045886361798d8ff041cc11443c9f3dade45e624a35f04cd58b58ae47b66ce8fa7f06ac7'
